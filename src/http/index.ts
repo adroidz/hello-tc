a2fbe3ba7e8a98980639e8c8e957c52aea3604c2
xiaoz <zxb@loveorange.com>
2023-04-02 11:23:42 +0800

    feat: add koa-bodyparser

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,11 +1,16 @@
 import Koa from 'koa'
 import KoaLogger from 'koa-logger'
 import KoaJson from 'koa-json'
+import KoaBodyparser from 'koa-bodyparser'
 import config from '../config'
 
 const app = new Koa()
 
 app.use(KoaLogger())
+
+app.use(KoaBodyparser({
+    enableTypes: ["json"]
+}))
 
 if(config.isDebugMode){
     app.use(KoaJson({pretty: true}))
@@ -13,7 +18,8 @@ if(config.isDebugMode){
 
 app.use(async(ctx)=>{
     ctx.body = {
-        text: 'Hello koa'
+        text: 'Hello koa',
+        body: ctx.request.body
     }
 })
 
