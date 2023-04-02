be6338e864aba98bb8595ce6c22881b44901b870
xiaoz <zxb@loveorange.com>
2023-04-02 10:25:00 +0800

    feat: add koa-json to format output json if debug mode

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,12 +1,20 @@
 import Koa from 'koa'
 import KoaLogger from 'koa-logger'
+import KoaJson from 'koa-json'
+import config from '../config'
 
 const app = new Koa()
 
 app.use(KoaLogger())
 
+if(config.isDebugMode){
+    app.use(KoaJson({pretty: true}))
+}
+
 app.use(async(ctx)=>{
-    ctx.body = 'Hello koa'
+    ctx.body = {
+        text: 'Hello koa'
+    }
 })
 
 export default app
